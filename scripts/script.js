ceec4ba1f773234bc0b0bbb615f7a66e650a4553
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-03-16 23:54:31 -0300

    Home atualizada

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 let timer = 0;
-			let image = 0;
+let image = 0;
 		
-			for(let i = 0; i < 3; i++) {
-				timer++;
-				console.log(timer);
-				if(timer >= 3) {
-					image++;
-					timer = 0;
-				}
+for(let i = 0; i < 3; i++) {
+	timer++;
+	console.log(timer);
+	if(timer >= 3) {
+		image++;
+		timer = 0;
+	}
 		
-				if(image >= 3) {
-					image = 0;
-				}
-				if(image == 0) {
-				document.getElementById("image1").style.display = "none";
-			}
-		}
+	if(image >= 3) {
+		image = 0;
+	}
+	if(image == 0) {
+    	document.getElementById("image1").style.display = "none";
+    }
+}
